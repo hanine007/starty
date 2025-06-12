32ce9b2fddf82bffec59534d99e7c8a1db5da352
hanine007 <bouchibahanine@gmail.com>
2025-06-12 20:21:18 +0100

    add morgan  http check

--- backend/server.js
+++ backend/server.js
@@ -2,11 +2,13 @@ import express from 'express';
 import  cors from 'cors';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import morgan from 'morgan';
 dotenv.config();
 const app = express();
 const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
+app.use(morgan('dev'));
 mongoose.connect(process.env.MONGODB_URI)
 .then( ()=>{
     console.log("Connected to MongoDB")
