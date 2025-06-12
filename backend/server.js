1b530d47c3de88b2b20e465d7ed8f1cb00fd57a6
hanine007 <bouchibahanine@gmail.com>
2025-06-12 17:02:14 +0100

    server

---
+++ backend/server.js
@@ -0,0 +1,19 @@
+import express from 'express';
+import  cors from 'cors';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
+const app = express();
+const port = process.env.PORT || 5000;
+app.use(cors());
+app.use(express.json());
+mongoose.connect(process.env.MONGODB_URI)
+.then( ()=>{
+    console.log("Connected to MongoDB")
+})
+.catch((err) => {
+    console.error("Error connecting to MongoDB:", err);
+});
+app.listen(port ,()=>{
+    console.log(`Server is running on port ${port}`);
+})
