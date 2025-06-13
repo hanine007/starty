376043d058964f36a647c5a6745cd86825591c56
hanine007 <bouchibahanine@gmail.com>
2025-06-13 11:56:07 +0100

    add a commit

--- backend/models/scpi.js
+++ backend/models/scpi.js
@@ -5,7 +5,7 @@ const ScpiSchema = new mongoose.Schema({
   societeId: {
     type: mongoose.Schema.Types.ObjectId,
     ref: 'Societe',
-    required: true
+    required: true // elle ne peut pas etre cree sans une societe
   }
 });
 
