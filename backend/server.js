b03560b2256928b9d37767c6f8e1f6e3220ffb47
hanine007 <bouchibahanine@gmail.com>
2025-06-13 12:33:16 +0100

    add route

--- backend/server.js
+++ backend/server.js
@@ -3,12 +3,24 @@ import  cors from 'cors';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import morgan from 'morgan';
+import scpisRoutes from './routes/scpis.js';
+import societesRoutes from './routes/societes.js';
 dotenv.config();
 const app = express();
 const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 app.use(morgan('dev'));
+// Route pour la racine
+app.get('/', (req, res) => {
+  res.send(' Bienvenue!');
+});
+
+// Routes
+app.use('/api/scpis', scpisRoutes);
+app.use('/api/societes', societesRoutes);
+
+
 mongoose.connect(process.env.MONGODB_URI)
 .then( ()=>{
     console.log("Connected to MongoDB")
@@ -16,6 +28,7 @@ mongoose.connect(process.env.MONGODB_URI)
 .catch((err) => {
     console.error("Error connecting to MongoDB:", err);
 });
-app.listen(port ,()=>{
-    console.log(`Server is running on port ${port}`);
-})
+
+app.listen(port, () => {
+  console.log(` Serveur démarré sur http://localhost:${port}`);
+});
