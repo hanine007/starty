bec285529b8f411edbd2bf346563260921a03597
hanine007 <bouchibahanine@gmail.com>
2025-06-14 02:28:15 +0100

    add route

--- frontend/frontend/src/App.jsx
+++ frontend/frontend/src/App.jsx
@@ -1,11 +1,22 @@
-
-import './App.css'
-
+import Home from "./pages/Home"
+import Login from "./pages/Login"
+import AdminPanel from "./pages/AdminPanel"
+import Layout from "./components/Layout"
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 function App() {
 
 
   return (
     <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path="admin/login" element={<Login />} />
+            <Route path="admin/panel" element={<AdminPanel />} />
+          </Route>
+        </Routes>
+      </Router>
       
     </>
   )
