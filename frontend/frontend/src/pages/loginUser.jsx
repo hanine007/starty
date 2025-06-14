883d4dabe0e0e3ec023c7b099ef225848fb9585b
hanine007 <bouchibahanine@gmail.com>
2025-06-14 14:56:16 +0100

    login user

---
+++ frontend/frontend/src/pages/loginUser.jsx
@@ -0,0 +1,36 @@
+// pages/LoginUser.jsx
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import api from '../services/api';
+
+const LoginUser = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [erreur, setErreur] = useState('');
+  const navigate = useNavigate();
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    try {
+      const res = await api.post('/user/login', { email, password });
+      localStorage.setItem('userToken', res.data.token);
+      navigate('/');
+    } catch {
+      setErreur(' Identifiants incorrects');
+    }
+  };
+
+  return (
+    <div style={{ maxWidth: '400px', margin: '2rem auto' }}>
+      <h2>Connexion utilisateur</h2>
+      {erreur && <p style={{ color: 'red' }}>{erreur}</p>}
+      <form onSubmit={handleLogin}>
+        <input type="email" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)} required />
+        <input type="password" placeholder="Mot de passe" value={password} onChange={(e) => setPassword(e.target.value)} required />
+        <button type="submit">Se connecter</button>
+      </form>
+    </div>
+  );
+};
+
+export default LoginUser;
