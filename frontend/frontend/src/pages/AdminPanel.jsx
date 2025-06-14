db099a37b7ec973a1b8255425c77d86595fa4eca
hanine007 <bouchibahanine@gmail.com>
2025-06-14 02:28:00 +0100

    pages

---
+++ frontend/frontend/src/pages/AdminPanel.jsx
@@ -0,0 +1,9 @@
+const AdminPanel = () => {
+  return (
+    <div>
+      <h2>Panneau d'administration</h2>
+    </div>
+  );
+};
+
+export default AdminPanel;
---
+++ frontend/frontend/src/pages/Login.jsx
@@ -0,0 +1,9 @@
+const Login =()=>{
+    return (
+        <>
+        <h2>Connexion Admin </h2>
+        
+        </>
+    )
+}
+export default Login;
