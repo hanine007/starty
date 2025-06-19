25c84a0f1abbce854c59e73974b03c3fe4f32d48
hanine007 <bouchibahanine@gmail.com>
2025-06-19 14:30:02 +0100

    add state

--- frontend/frontend/src/pages/Home.jsx
+++ frontend/frontend/src/pages/Home.jsx
@@ -1,4 +1,20 @@
+import { useState,useEffect } from  "react";
+import api from "../services/api";
+
+
+
 const Home =()=>{
+    const [scpis, setScpis] = useState([]);
+    const [societes, setSocietes] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+    // etats des filtres
+    const [searchTerm,setSearchTerm] = useState("");
+    const [selectedSociete, setSelectedSociete] = useState("");
+    const [rendementMax, setRendementMax] = useState("");
+    const [rendementMin, setRendementMin] = useState("");
+    
+    
     return (
         <div>
             <h2>Listes des SCPI et les filter </h2>
