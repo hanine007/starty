e04e81ed1d919cecdeee86f990453edd090e878f
hanine007 <bouchibahanine@gmail.com>
2025-06-19 14:37:57 +0100

    fetch data

--- frontend/frontend/src/pages/Home.jsx
+++ frontend/frontend/src/pages/Home.jsx
@@ -13,8 +13,28 @@ const Home =()=>{
     const [selectedSociete, setSelectedSociete] = useState("");
     const [rendementMax, setRendementMax] = useState("");
     const [rendementMin, setRendementMin] = useState("");
-    
-    
+    // charger les scpis et les societes au chargement de la page 
+    useEffect(()=>{
+        const fetchData= async ()=>{
+            setLoading(true);
+            try{
+                const [scpiResponse, societeResponse] = await Promise.all([
+                    api.get('/scpis'),
+                    api.get('/societes')
+                ]);
+                setScpis(scpiResponse.data);
+                setSocietes(societeResponse.data);
+                setError(null);
+            } catch (error) {
+                console.error("Erreur lors du chargement des données :", error);
+                setError("Une erreur s'est produite lors du chargement des données.");
+            } finally {
+                setLoading(false);
+            }
+        };
+        fetchData();
+    }, []);
+
     return (
         <div>
             <h2>Listes des SCPI et les filter </h2>
